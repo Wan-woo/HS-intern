6a1abc48abdfc6149ca3466435c9444b7d4b5e76
Wan-woo <15056032198@163.com>
2019-07-10 13:24:14 +0800

    增加了消息框

--- myFirstWindow.py
+++ myFirstWindow.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(400, 300)
+        Form.resize(423, 315)
         self.gridLayout = QtWidgets.QGridLayout(Form)
         self.gridLayout.setObjectName("gridLayout")
         self.pushButton = QtWidgets.QPushButton(Form)
@@ -19,14 +19,21 @@ class Ui_Form(object):
         self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
         self.textEdit = QtWidgets.QTextEdit(Form)
         self.textEdit.setObjectName("textEdit")
-        self.gridLayout.addWidget(self.textEdit, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.textEdit, 1, 0, 1, 3)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 0, 2, 1, 1)
+        self.exitButton = QtWidgets.QPushButton(Form)
+        self.exitButton.setObjectName("exitButton")
+        self.gridLayout.addWidget(self.exitButton, 0, 1, 1, 1)
 
         self.retranslateUi(Form)
         self.pushButton.clicked.connect(Form.pushButton_click)
+        self.exitButton.clicked.connect(Form.exitButton_click)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.pushButton.setText(_translate("Form", "PushButton"))
+        self.exitButton.setText(_translate("Form", "Exit"))
 
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtGui import QIcon
 from myFirstWindow import Ui_Form
+
+
 
 class MyForm(QtWidgets.QWidget, Ui_Form):
     def __init__(self):
@@ -12,6 +15,16 @@ class MyForm(QtWidgets.QWidget, Ui_Form):
 
     def pushButton_click(self):
         self.textEdit.setText("你点击了按钮")
+
+    def exitButton_click(self):
+        reply = QMessageBox.question(self, 'Message',
+                                     "Are you sure to quit?", QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            quit()
+        else:
+            return
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
