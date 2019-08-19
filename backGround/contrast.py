f1807f1fb73b275f555d4de796e7abde6243d1f2
wendenggo <zwdeng@163.com>
2019-08-19 16:29:48 +0800

    修改根据模块名获取对象

--- backGround/contrast.py
+++ backGround/contrast.py
@@ -12,6 +12,13 @@
 from backGround.backupSql import getBackupTime,getObjectByVersion
 from backGround.setupSql import tuplesToList
 from backGround.testConnection import getSqliteConnection,getOrcaleConnection
+def fieldListToStr(fieldList):
+    fieldStr=""
+    for index in range(len(fieldList)):
+        fieldStr+=fieldList[index]
+        if len(fieldList)-1>index:
+            fieldStr+=','
+    return fieldStr
 def makeContrasr(backupVersion):
     [beginTime,endTime]=getBackupTime(backupVersion)
     objectList = getObjectByVersion(backupVersion)
@@ -20,12 +27,12 @@ def makeContrasr(backupVersion):
     sqliteConn = getSqliteConnection()
     sqliteCursor = sqliteConn.cursor()
     for object in objectList:
-        if object[1]:
+        if object[1]==1:
             tableList.append(object[0])
             backupTableList.append(object[2])
     for index in range(len(tableList)):
-        keySql = "select fieldChosed from backupFieldKey where tableName = %s and fieldType = 2 "%(tableList[index])
-        fieldSql = "select fieldChosed from backupFieldKey where tableName = %s and fieldType = 2 "%(tableList[index])
+        keySql = "select fieldChosed from backupFieldKey where tableName = '%s' and fieldType = 2 "%(tableList[index])
+        fieldSql = "select fieldChosed from backupFieldKey where tableName = '%s' "%(tableList[index])
         sqliteCursor.execute(keySql)
         keyList = sqliteCursor.fetchall()
         keyList = tuplesToList(keyList)
@@ -34,16 +41,35 @@ def makeContrasr(backupVersion):
         fieldList = tuplesToList(sqliteCursor.fetchall())
         fieldStr = fieldListToStr(fieldList)
 
-        deleteSql = 'select "%s" from "%s" MINUS select "%s" from "%s" where FDATE BETWEEN "%s"  AND "%s"'%(keyList,tableList[index],keyList,backupTableList[index],beginTime,endTime)
-        insertSql = 'select "%s" from "%s" MINUS select "%s" from "%s" where FDATE BETWEEN "%s"  AND "%s"'%(keyList,backupTableList[index],keyList,tableList[index],beginTime,endTime)
-        sameSql = 'select "%s" from "%s" intersect select "%s" from "%s" where FDATE BETWEEN "%s"  AND "%s"'%(fieldList,tableList[index],fieldList,backupTableList[index],beginTime,endTime)
-        updateSql = '(select "%s" from "%s" intersect select "%s" from "%s" where FDATE BETWEEN "%s"  AND "%s")minus (%s)'%(fieldList,tableList[index],fieldList,backupTableList[index],beginTime,endTime,sameSql)
+        deleteSql = 'select %s from %s MINUS select %s from %s where FDATE BETWEEN %s  AND %s'%(keyStr,tableList[index],keyStr,backupTableList[index],beginTime,endTime)
+        insertSql = 'select %s from %s MINUS select %s from %s where FDATE BETWEEN %s  AND %s'%(keyStr,backupTableList[index],keyStr,tableList[index],beginTime,endTime)
+        sameSql = 'select %s from (select %s from %s intersect select %s from %s where FDATE BETWEEN %s  AND %s)'%(keyStr,fieldStr,tableList[index],fieldStr,backupTableList[index],beginTime,endTime)
+        updateSql = '(select %s from %s intersect select %s from %s where FDATE BETWEEN %s  AND %s)minus ' \
+                    '(select %s from (%s))'%(keyStr,tableList[index],keyStr,backupTableList[index],beginTime,endTime,keyStr,sameSql)
+        oracleConn = getOrcaleConnection()
+        oracleCursor = oracleConn.cursor()
+
+        oracleCursor.execute(deleteSql)
+        deleteList = oracleCursor.fetchall()
+        deleteList = tuplesToList(deleteList)
+
+        oracleCursor.execute(insertSql)
+        insertList = oracleCursor.fetchall()
+        insertList = tuplesToList(insertList)
+
+        oracleCursor.execute(sameSql)
+        sameList = oracleCursor.fetchall()
+        sameList = tuplesToList(sameList)
+
+        oracleCursor.execute(updateSql)
+        updateList = oracleCursor.fetchall()
+        updateList = tuplesToList(updateList)
+        print(updateSql)
+    return deleteList,insertList,sameList,updateList
+contrasrList = makeContrasr(5)
+print(contrasrList[0])
+print(contrasrList[1])
+print(contrasrList[2])
+print(contrasrList[3])
 
 
-
-
-def fieldListToStr(fieldList):
-    fieldStr=""
-    for field in fieldList:
-        fieldList+=field
-    return fieldStr
